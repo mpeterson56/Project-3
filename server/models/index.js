d8e34c6e5dacbb13950689bfed7d4985d8086fd3
GhulamZaka <100795524+GhulamZaka@users.noreply.github.com>
2022-08-18 14:17:05 -0700

    adding models and schemas

---
+++ server/models/index.js
@@ -0,0 +1,8 @@
+const Assignment = require('./Assignment');
+const Bids = require('./Bids');
+const Client = require('./Client');
+const Subject = require('./Subject');
+const Tutor = require('./Tutor');
+const User = require('./User');
+
+module.exports = { Assignment, Bids, Client, Subject, Tutor, User };
