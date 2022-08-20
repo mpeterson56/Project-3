2c8cb429f8cdd11357bbfbded57315a701c74ac4
Macy Kay Mannix <macymannix@gmail.com>
2022-08-20 12:34:35 -0700

    student tutor portals

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,45 +1,47 @@
-import React, { Component } from 'react';
-import Logout from '../Logout.js';
-import { Link } from 'react-router-dom';
+// import React, { Component } from 'react';
 
-class Nav extends Component {
-  render(){
-    let links = <span />;
-    if(this.props.user){
-      links = (
-        <span>
-          <Link to="/saved/profile">Profile</Link>
-          <Logout updateUser={this.props.updateUser} />
-        </span>
-      );
-    }
-    else {
-      links = (
-        <span>
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </span>
-      );
-    }
+// import { Link } from 'react-router-dom';
 
-    return(
-      <div className="navbar-fixed">
-        <nav className="transparent z-depth-0">
-          <div className="nav-wrapper">
-            <div className="col s12">
-              <a href="/">Home</a>
-              <Link to="/jobs">Jobs</Link>
-              <Link to="/account">Account</Link>
-                {links}
-            </div>
-          </div>
-        </nav>
-        <header>
+// import Logout from '../Logout.js';
+
+// class Nav extends Component {
+//   render(){
+//     let links = <span />;
+//     if(this.props.user){
+//       links = (
+//         <span>
+//           <Link to="/saved/profile">Profile</Link>
+//           <Logout updateUser={this.props.updateUser} />
+//         </span>
+//       );
+//     }
+//     else {
+//       links = (
+//         <span>
+//           <Link to="/login">Login</Link>
+//           <Link to="/signup">Sign Up</Link>
+//         </span>
+//       );
+//     }
+
+//     return(
+//       <div className="navbar-fixed">
+//         <nav className="transparent z-depth-0">
+//           <div className="nav-wrapper">
+//             <div className="col s12">
+//               <a href="/">Home</a>
+//               <Link to="/jobs">Jobs</Link>
+//               <Link to="/account">Account</Link>
+//                 {links}
+//             </div>
+//           </div>
+//         </nav>
+//         <header>
     
-        </header>
-      </div>
-    );
-  }
-}
+//         </header>
+//       </div>
+//     );
+//   }
+// }
 
-export default Nav;
+// export default Nav;
